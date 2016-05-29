002758d2b7b21e1cd6c8ba2f736222c5c5ba5a5b
Sebastien Descahmps <sebastien.deschamps@eleves.enpc.fr>
2016-05-29 15:37:08 +0200

    imports utiles

---
+++ arbre.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt # module pour les outils graphiques
+import tools # module fourni en TP1
+from sklearn import tree # module pour les arbres
+from sklearn import ensemble # module pour les forets
+from sklearn import cross_validation as cv
+from IPython.display import Image
+import pydot
---
+++ discreteAdaboost.py
@@ -0,0 +1,1 @@
+from arbre.py import *
